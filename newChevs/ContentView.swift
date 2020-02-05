8589719b1cbdda1397779b4f66228509ad88c31b
Colton Lipchak <clipchak@gmail.com>
2020-02-05 18:26:22 -0500

    Initial Commit

---
+++ newChevs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newChevs
+//
+//  Created by Colton Lipchak on 2/5/20.
+//  Copyright © 2020 clipchak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
